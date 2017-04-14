9f7c4a62e9ccbd21357347f67307314362990cc7
Hossein Abedi <abedi.hossein@protonmail.ch>
2017-04-14 22:50:25 +0430

    updated main.py

--- main.py
+++ main.py
@@ -6,17 +6,11 @@ if __name__ == "__main__":
     offset = 0
     size = 10
     rounds = 1
-
-    df = pd.DataFrame()
-
+    
     for i in range(rounds):
-         data = ElasticCom(index='index', host="176.9.8.171",
+         data = ElasticCom(index='index-name', host="localhost",
                       port=9200, username="elastic", password="changeme",
                       authentication=True,
                       doc_type='doc', size=10, from_=0).search_and_export_to_df()
 
-         with open("content.txt", "a") as myfile:
-               for line in data["content"].values:
-                        myfile.write(line + "\n")
-    
-
+         
